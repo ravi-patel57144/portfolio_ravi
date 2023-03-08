497a7cb631ed98e0acebaa6981dbed614f587a3e
Ravi SIngh Patel <rprs80115@gmail.com>
2023-03-09 01:20:07 +0530

    few glitches and errors removed

--- dash/admin.py
+++ dash/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, Docs
+
 # Register your models here.
 
 admin.site.register(Profile)
+admin.site.register(Docs)
--- dash/models.py
+++ dash/models.py
@@ -1,3 +1,7 @@
+import os
+
+from django.conf import settings
+from django.core.validators import FileExtensionValidator
 from django.db import models
 
 
@@ -5,3 +9,10 @@ from django.db import models
 class Profile(models.Model):
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=500)
+
+
+class Docs(models.Model):
+    resume = models.FileField(upload_to='resume/', validators=[FileExtensionValidator(['pdf'])],
+                              help_text='File Must Be in PDF format')
+    github = models.CharField(max_length=200, blank=True, null=True)
+    linkedin = models.CharField(max_length=200, blank=True, null=True)
