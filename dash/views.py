5c0cbc3f40fcc90105d5b9865aec15f5daa7e1e2
Ravi SIngh Patel <rprs80115@gmail.com>
2023-05-14 13:30:20 +0530

    admin css

--- dash/views.py
+++ dash/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 
-from .models import Profile
+from .models import Profile, Docs
 
 
 # Create your views here.
 def home(request):
     profile = Profile.objects
-    return render(request, 'index.html', {'profile': profile})
+    docs = Docs.objects.filter().first()
+    return render(request, 'index.html', {'profile': profile, 'docs': docs})
